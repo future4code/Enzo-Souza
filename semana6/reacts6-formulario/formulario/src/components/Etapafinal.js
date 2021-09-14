a7671ea4b6ba5fcc45a14a297d52dad07f80924a
Enzo Emiliano Silva de Souza <enzoemiliano96@gmail.com>
2021-09-13 23:14:08 -0300

    semana6

---
+++ semana6/reacts6-formulario/formulario/src/components/Etapafinal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default class Etapafinal extends React.Component {
+  render () {
+  return (
+        <div>
+         <h2> O FORMULÁRIO ACABOU  </h2>
+         <h3> Muito obrigado por participar! </h3>
+        </div>
+  )};
+}
+      
+
